73b07b6f9e80da9bb95c83935d01213acbd5d93a
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-06-16 15:41:40 -1000

    Implement chart.js

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,86 +1,118 @@
 import React, { useState, useEffect } from "react";
-import {useParams} from 'react-router-dom';
-import '../css/Display.css'
-
+import { useParams } from "react-router-dom";
+import "../css/Display.css";
 
 var axios = require("axios").default;
 
 const Display = () => {
- const {elementSymbol} = useParams()
+  const { elementSymbol } = useParams();
   const [post, setPost] = useState([]);
   const [data, setData] = useState([]);
-  const[stockPrice, setStockPrice] = useState([])
-  const[aboutStock, setAboutStock] = useState([])
-  const[stockFinancials, setStockFinancials] = useState([])
+  const [stockPrice, setStockPrice] = useState([]);
+  const [aboutStock, setAboutStock] = useState([]);
+  const [stockFinancials, setStockFinancials] = useState([]);
+  const [rating, setRating] = useState([]);
+  const [grade, setGrade] = useState([])
 
-//API Calls
+  //API Calls
 
   //Getting the P/E ratio
   useEffect(() => {
-      axios
-        .get(`https://financialmodelingprep.com/api/v3/ratios-ttm/${elementSymbol}?apikey=${process.env.REACT_APP_API_KEY}`)
-        .then((response) => {
-          console.log("responsedata", response.data[0])
-          setPost(response.data[0]);
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
+    axios
+      .get(
+        `https://financialmodelingprep.com/api/v3/ratios-ttm/${elementSymbol}?apikey=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((response) => {
+        console.log("responsedata", response.data[0]);
+        setPost(response.data[0]);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   }, []);
 
   //Getting the stock price
   useEffect(() => {
     axios
-      .get(`https://financialmodelingprep.com/api/v3/quote/${elementSymbol}?apikey=${process.env.REACT_APP_API_KEY}`)
+      .get(
+        `https://financialmodelingprep.com/api/v3/quote/${elementSymbol}?apikey=${process.env.REACT_APP_API_KEY}`
+      )
       .then((response) => {
-        console.log("responsedata", response.data[0])
+        console.log("responsedata", response.data[0]);
         setStockPrice(response.data[0]);
       })
       .catch(function (error) {
         console.error(error);
       });
-}, []);
+  }, []);
 
-//Getting the company description
-useEffect(() => {
-  axios
-    .get(`https://financialmodelingprep.com/api/v3/profile/${elementSymbol}?apikey=${process.env.REACT_APP_API_KEY}`)
-    .then((response) => {
-      console.log("responsedata", response.data[0])
-      setAboutStock(response.data[0]);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-}, []);
+  //Getting the company description
+  useEffect(() => {
+    axios
+      .get(
+        `https://financialmodelingprep.com/api/v3/profile/${elementSymbol}?apikey=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((response) => {
+        console.log("responsedata", response.data[0]);
+        setAboutStock(response.data[0]);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
 
-//Getting the EPS from this endpoint
-useEffect(() => {
-  axios
-    .get(`https://financialmodelingprep.com/api/v3/income-statement/${elementSymbol}?limit=120&apikey=${process.env.REACT_APP_API_KEY}`)
-    .then((response) => {
-      console.log("responsedata", response.data[0])
-      setStockFinancials(response.data[0]);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-}, []);
+  //Getting the EPS from this endpoint
+  useEffect(() => {
+    axios
+      .get(
+        `https://financialmodelingprep.com/api/v3/income-statement/${elementSymbol}?limit=120&apikey=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((response) => {
+        console.log("responsedata", response.data[0]);
+        setStockFinancials(response.data[0]);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
 
+  //Getting the share rating
 
+  useEffect(() => {
+    axios
+      .get(
+        `https://financialmodelingprep.com/api/v3/rating/${elementSymbol}?apikey=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((response) => {
+        console.log("responsedata", response.data[0]);
+        setRating(response.data[0]);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
 
+  //Grade of each stock
+  useEffect(() => {
+    axios
+      .get(
+        `https://financialmodelingprep.com/api/v3/grade/${elementSymbol}?limit=10&apikey=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((response) => {
+        console.log("responsedata", response.data[0]);
+        setGrade(response.data[0]);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
 
-
-  
   return (
     <div className="dataInfo">
       <h2 id="stockName">{stockPrice.name}</h2>
       <h3 id="stockPrice">Stock Price:{` $` + stockPrice.price}</h3>
       <p>{aboutStock.description}</p>
-      <h5>
-        Average P/E Ratio Current Year:{" "}
-        ${post.peRatioTTM}
-      </h5>
+      <h5>Average P/E Ratio Current Year: ${post.peRatioTTM}</h5>
       <p>
         What is P/E ratio? The Price/Earnings ratio is used to determine how
         much investors are willing to pay for a stock relative to the company's
@@ -89,10 +121,7 @@ useEffect(() => {
         determines the bargain you can get for each stock. In most cases, the
         lower the P/E ratio, the better.{" "}
       </p>
-      <h5>
-        Earnings Per Share Current Year:{" "}
-        ${stockFinancials.eps}
-      </h5>
+      <h5>Earnings Per Share Current Year: ${stockFinancials.eps}</h5>
       <p>
         Earnings per share is how much the company earned during the quarter for
         1 share of stock. As a general rule of thumb, the higher the EPS the
@@ -100,7 +129,7 @@ useEffect(() => {
         one share of that stock.{" "}
       </p>
       <h5>
-        Determining Industry Average Price:{" "}
+        Determining Intrinsic Average Price:{" "}
         {`$` + post.peRatioTTM * stockFinancials.eps}
       </h5>
       <p>
@@ -111,12 +140,14 @@ useEffect(() => {
         ratio is far off from the industry average, it might eventually - but
         not necessarily - realign with the industry average."
       </p>
-      <h5>
-        Average Analyst Rating:{" "}
-        {data.averageAnalystRating ||
-          post.averageAnalystRating}
-      </h5>
-      <h5>52 Week Range: { data.fiftyTwoWeekRange  || post.fiftyTwoWeekRange}</h5>
+      <h6>Average Analyst Rating as of {rating.date}:{" " + rating.ratingDetailsDCFScore + " "}</h6>
+      <h6>
+        DCF (Discounted Cash Flow) Reccommendation as of {rating.date}:{" "}
+        {rating.ratingDetailsDCFRecommendation}
+      </h6>
+      <h6>52 Week Range: {aboutStock.range}</h6>
+      <h6>Grade as of {grade.date} by {grade.gradingCompany}: {grade.newGrade} </h6>
+      <p>**The grade info is pulled from most recent company that has graded the stock.**</p>
       <button id="watchlist">Add To Watchlist</button>
     </div>
   );
--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {Chart as ChartJS} from 'chart.js/auto'
 
 const Graph = () => {
   return (
-    <div>Each Stock Graph will go here!</div>
-  )
-}
+    <div style={{height:'800px', width:'50%'}}>
+      <Line
+        const data = {{
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+              label: 'My First Dataset',
+              data: [10, 5, 3, 7, 8, , 40],
+              fill: false,
+              borderColor: 'rgb(75, 192, 192)',
+              tension: 0.1
+            }]
+          }}
+          options={{
+            maintainAspectRatio: false,
+          }}
+          
+      />
+    </div>
+  );
+};
 
-export default Graph
+export default Graph;
