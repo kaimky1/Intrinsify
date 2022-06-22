b1b4a8bb5a37ab880d5943f58ae6d1eb0985a079
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-06-21 18:10:08 -1000

    CSS Styling, Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,128 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { render } from "react-dom";
+import HomeCSS from "../css/Home.module.css";
+var axios = require("axios").default;
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [post, setPost] = useState([]);
 
-export default Home
+  useEffect(() => {
+    axios
+      .get(
+        `https://financialmodelingprep.com/api/v3/quote/%5EGSPC,%5EDJI,%5EIXIC?apikey=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setPost(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  //   if(post.length > 0){
+
+  //       let stocks = () => {
+  //         return(
+  //             <div>
+  //             <h1 className={HomeCSS.header}>Stock Market Index</h1>
+  //           <div className={HomeCSS.wrapper}>
+  //             <div className={HomeCSS.table}>
+  //               <div className={`${HomeCSS.row} ${HomeCSS.header}`}>
+  //                 <div className={HomeCSS.cell}>Name</div>
+  //                 <div className={HomeCSS.cell}>Price</div>
+  //                 <div className={HomeCSS.cell}>Percent Change</div>
+  //                 <div className={HomeCSS.cell}>Change</div>
+  //               </div>
+
+  //               <div className={HomeCSS.row}>
+  //                 <div className={HomeCSS.cell} data-title={HomeCSS.Name}>
+  //                   {post[0].name}
+  //                 </div>
+  //                 <div className={HomeCSS.cell} data-title={HomeCSS.Age}>
+  //                   ${post[0].price}
+  //                 </div>
+  //                 <div className={HomeCSS.cell} data-title={HomeCSS.Occupation}>
+  //                   {post[0].changesPercentage}%
+  //                 </div>
+  //                 <div className={HomeCSS.cell} data-title={HomeCSS.Location}>
+  //                   ${post[0].change}
+  //                 </div>
+  //               </div>
+
+  //               <div className={HomeCSS.row}>
+  //                 <div className={HomeCSS.cell} data-title={HomeCSS.Name}>
+  //                   {post[1].name}
+  //                 </div>
+  //                 <div className={HomeCSS.cell} data-title={HomeCSS.Age}>
+  //                 ${post[1].price}
+  //                 </div>
+  //                 <div className={HomeCSS.cell} data-title={HomeCSS.Occupation}>
+  //                 {post[1].changesPercentage}%
+  //                 </div>
+  //                 <div className={HomeCSS.cell} data-title={HomeCSS.Location}>
+  //                 ${post[1].change}
+  //                 </div>
+  //               </div>
+
+  //               <div className={HomeCSS.row}>
+  //                 <div className={HomeCSS.cell} data-title={HomeCSS.Name}>
+  //                   {post[2].name}
+  //                 </div>
+  //                 <div className={HomeCSS.cell} data-title={HomeCSS.Age}>
+  //                 ${post[2].price}
+  //                 </div>
+  //                 <div className={HomeCSS.cell} data-title={HomeCSS.Occupation}>
+  //                 {post[2].changesPercentage}%
+  //                 </div>
+  //                 <div className={HomeCSS.cell} data-title={HomeCSS.Location}>
+  //                 ${post[2].change}
+  //                 </div>
+  //               </div>
+  //               </div>
+  //             </div>
+  //             </div>
+  //         )
+  //       }
+  //   }
+
+  let stocks = post.map((element, index) => {
+    return (
+      <div>
+        <div className={HomeCSS.row}>
+          <div className={HomeCSS.cell} data-title={HomeCSS.Name}>
+            {element.name}
+          </div>
+          <div className={HomeCSS.cell} data-title={HomeCSS.Age}>
+            ${element.price}
+          </div>
+          <div className={HomeCSS.cell} data-title={HomeCSS.Occupation}>
+            {element.changesPercentage}%
+          </div>
+          <div className={HomeCSS.cell} data-title={HomeCSS.Location}>
+            ${element.change}
+          </div>
+        </div>
+      </div>
+    );
+  });
+  console.log(stocks, "stocks")
+  return (
+    <div>
+        <h1 className={HomeCSS.header}>Stock Market Index</h1>
+      <div className={HomeCSS.wrapper}>
+        <div className={HomeCSS.table}>
+          <div className={`${HomeCSS.row} ${HomeCSS.header}`}>
+            <div className={HomeCSS.cell}>Name</div>
+            <div className={HomeCSS.cell}>Price</div>
+            <div className={HomeCSS.cell}>Percent Change</div>
+            <div className={HomeCSS.cell}>Change</div>
+          </div> 
+          </div>
+            {stocks}
+        </div>
+      </div>
+  )
+};
+
+export default Home;
--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import { render } from "@testing-library/react";
+import React, { useEffect, useState, prevState } from "react";
 
 var axios = require("axios").default;
 let userID = Number(window.localStorage.getItem("userID"));
@@ -6,27 +7,57 @@ let userID = Number(window.localStorage.getItem("userID"));
 const Watchlist = () => {
   const [post, setPost] = useState([]);
   const [stock, setStock] = useState([]);
- 
-useEffect(() => {
-    axios.get(`http://localhost:4004/getFavorite`, {
-      params: {
-        userID: userID,
-      },
-    })
-    .then((res) => {
-      setPost(res.data)
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }, [])
-  console.log(post)
-post.map((element, index) => {
-  
-})
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4004/getFavorite`, {
+        params: {
+          userID: userID,
+        },
+      })
+      .then((res) => {
+        setPost(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+
+    const stocks = post.map((element, index) => {
+        return(
+          <h6 key={index}>{element.stock_ticker}</h6>
+        )
+    }
+    );
+
+    const getPrice = async() => {
+      let prices = await Promise.all(
+        post.map(async(element, index) => {
+          await axios.get(`https://financialmodelingprep.com/api/v3/quote/${element.stock_ticker}?apikey=${process.env.REACT_APP_API_KEY}`)
+          .then(res => console.log(res.data))
+        })
+      )
+    }
+    // let prices = post.map(async (element, index) => { await
+    //     axios.get(`https://financialmodelingprep.com/api/v3/quote/${element.stock_ticker}?apikey=${process.env.REACT_APP_API_KEY}`)
+    //     .then(res => {
+    //       console.log(res.data)
+    //     }) 
+    //     render(
+    //       <h1></h1>
+    //     )
+    //   })
+   
+
+
   return (
     <div>
       <h1>Watchlist</h1>
+      <div>
+        {stocks}
+        {getPrice}
+      </div>
     </div>
   );
 };
